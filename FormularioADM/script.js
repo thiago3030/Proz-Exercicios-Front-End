5c08c40563c35ddd75698e5a18a2304a6458bc8d
thiago <104877209+thiago3030@users.noreply.github.com>
2023-11-23 18:14:07 -0300

    Add files via upload

---
+++ FormularioADM/script.js
@@ -0,0 +1,8 @@
+// Adicione interatividade aqui, se necessário
+// Por exemplo, pode adicionar validações de formulário ou ações de botão
+
+document.getElementById('cadastroForm').addEventListener('submit', function(event) {
+    // Adicione lógica de validação ou processamento aqui
+    // Por exemplo, previna o envio do formulário se algum campo estiver vazio
+    // event.preventDefault();
+});
